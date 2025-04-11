14d36d8e25991de0fe11d511550bf096cd3775fa
carlos <carlos.rodrigues@evogest.fr>
2025-04-11 14:09:16 +0200

    Feature<App client>: creation app client

--- Django_api/django_project/settings.py
+++ Django_api/django_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',  # Assurez-vous que cette ligne existe
+    'api',  
 ]
 
 MIDDLEWARE = [
